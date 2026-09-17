f52b30151b9aee6ad3a2a1a7349f4baa5743fde2
llyufenggotest <yuyil7580@gmail.com>
2026-09-17 15:57:04 +0800

    fix: dart format compliance for paper_plane_status_icon

--- lib/views/dashboard/widgets/paper_plane_status_icon.dart
+++ lib/views/dashboard/widgets/paper_plane_status_icon.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 /// Paper plane icon that rotates based on connection state
 class PaperPlaneStatusIcon extends StatelessWidget {
-  final bool disconnected;
-
   const PaperPlaneStatusIcon({
     super.key,
     required this.disconnected,
   });
+
+  final bool disconnected;
 
   @override
   Widget build(BuildContext context) {
